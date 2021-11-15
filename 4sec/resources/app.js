ab2afdd4a52e3c7019d531a7aa50f4be08673114
th3nd <45330378+th3nd@users.noreply.github.com>
2021-11-15 14:50:19 +0100

    new amazing site

---
+++ 4sec/resources/app.js
@@ -0,0 +1,11 @@
+function startVid() {
+    document.getElementById('button').style.display ='none';
+    document.getElementById('wrap').style.display ='block';
+    document.getElementById("bg_vid").play();
+}
+
+function moreInfo() {
+    document.getElementById('wrap').style.display ='none';
+    document.getElementById('moreInfo').style.display ='inline-block';
+
+}
