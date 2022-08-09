16de986b4f5f880e29e57fd8c1535bfd54c8201e
th3nd <45330378+th3nd@users.noreply.github.com>
2022-08-09 19:04:06 +0200

    made a lil cleaner

--- script.js
+++ script.js
@@ -25,38 +25,44 @@ function validate(txt) {
     if (txt.includes("title")) {
         txt = txt.slice(6)
         document.title = (txt)
-        txt = "title"
+        txt = "succ"
+    }
+    
+    if (txt.includes("bg")) {
+        txt = txt.slice(3)
+        document.body.style.backgroundColor  = (txt)
+        txt = "succ" // kan ändras
     }
 
-    /* does not look nice */
     switch (txt) {
         case "help":
-            content.innerHTML += "<p style='color: rgb(100, 200, 100)'>" + "list of commands: " + "</p>"
-            content.innerHTML += "<p style='color: rgb(100, 100, 100)'>" + "help - what do u think" + "</p>"
-            content.innerHTML += "<p style='color: rgb(100, 100, 100)'>" + "acc - list of current steam accounts" + "</p>"
-            content.innerHTML += "<p style='color: rgb(100, 100, 100)'>" + "contact - contact information" + "</p>"
-            content.innerHTML += "<p style='color: rgb(100, 100, 100)'>" + "playlist - link to my playlist (wow)" + "</p>"
-            content.innerHTML += "<p style='color: rgb(100, 100, 100)'>" + "cls - clears the cmd." + "</p>"
-            content.innerHTML += "<p style='color: rgb(100, 100, 100)'>" + "title - sets the sites title" + "</p>"
+            content.innerHTML += "<p id='green'>" + "list of commands: " + "</p>"
+            content.innerHTML += "<p id='grey'>" + "help - what do u think" + "</p>"
+            content.innerHTML += "<p id='grey'>" + "acc - list of current steam accounts" + "</p>"
+            content.innerHTML += "<p id='grey'>" + "contact - contact information" + "</p>"
+            content.innerHTML += "<p id='grey'>" + "playlist - link to my playlist (wow)" + "</p>"
+            content.innerHTML += "<p id='grey'>" + "cls - clears the cmd" + "</p>"
+            content.innerHTML += "<p id='grey'>" + "title %text% - sets the sites title" + "</p>"
+            content.innerHTML += "<p id='grey'>" + "bg %color% - sets the sites bg-color" + "</p>"
             break
         case "acc":
-            content.innerHTML += "<p style='color: rgb(100, 200, 100)'>" + "list of accounts: " + "</>"
-            content.innerHTML += "<a href='https://steamcommunity.com/id/63u' style='color: rgb(100, 200, 100)'>" + "main" + "</a>"
-            content.innerHTML += "<a href='https://steamcommunity.com/id/leandoer2k' style='color: rgb(100, 200, 100)'>" + " | fresh" + "</a>"
-            content.innerHTML += "<a href='https://steamcommunity.com/id/srcengine' style='color: rgb(100, 200, 100)'>" + " | lvl 50+" + "</a>"
-            content.innerHTML += "<a href='https://steamcommunity.com/id/srcsdk' style='color: rgb(100, 200, 100)'>" + " | 2016 medal" + "</a>"
+            content.innerHTML += "<p id='green'>" + "list of accounts: " + "</>"
+            content.innerHTML += "<p onclick=link('steamcommunity.com/id/63u') id='l'>" + "main" + "</p>"
+            content.innerHTML += "<p onclick=link('steamcommunity.com/id/leandoer2k') id='l'>" + "fresh" + "</p>"
+            content.innerHTML += "<p onclick=link('steamcommunity.com/id/srcengine') id='l'>" + "lvl 50+" + "</p>"
+            content.innerHTML += "<p onclick=link('steamcommunity.com/id/srcsdk') id='l'>" + "2016 medal" + "</p>"
             break
         case "contact":
-            content.innerHTML += "<p style='color: rgb(100, 200, 100)'>" + "discord: thn#3690" + "</p>"
+            content.innerHTML += "<p id='green'>" + "discord: thn#3690" + "</p>"
             break
         case "playlist":
-            content.innerHTML += "<a href='https://open.spotify.com/playlist/7wf0WXwT4OwJNac3oHG4HO?si=cc1622b2366d4fd4' style='color: rgb(100, 200, 100)'>" + "click here" + "</a>"
+            content.innerHTML += "<p onclick=link('open.spotify.com/playlist/7wf0WXwT4OwJNac3oHG4HO?si=cc1622b2366d4fd4') id='l'>" + "click here" + "</p>"
             break
         case "cls":
             content.innerHTML = ""
             break
-        case "title":
-            content.innerHTML += "<p style='color: rgb(100, 200, 100)'>" + "success." + "</p>"
+        case "succ":
+            content.innerHTML += "<p id='green'>" + "success." + "</p>"
             break
         default:
             content.innerHTML += "<p style='color: rgb(200, 100, 100)'>" + "not a valid command. type 'help' for a list of commands." + "</p>"
@@ -64,3 +70,6 @@ function validate(txt) {
     }
 }
 
+function link(url) {
+    window.open("https://" + url)
+}
